ac5075ed28bae9b5f1b247b44b978ef9821dc893
Rigaud <bertrand.rigaud@cc.in2p3.fr>
2020-05-19 13:24:40 +0200

    facing environment

--- part-2/app.js
+++ part-2/app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: "environment" }, audio: false };
 var track = null;
 
 // Define constants
@@ -46,4 +46,3 @@ if ('serviceWorker' in navigator) {
 } else {
   console.log('CLIENT: service worker is not supported.');
 }
-
